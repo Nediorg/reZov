77affe8ab445d7b5efea4c475508cd41db17638a
nediorg <46892579+Nediorg@users.noreply.github.com>
2024-03-11 02:30:56 +0700

    Can now display an error (and reason) and also send a loading message

--- src/Extensions/search.py
+++ src/Extensions/search.py
@@ -1,16 +1,33 @@
-# Configs
-librex_instance = 'https://search.davidovski.xyz/api.php?q='
-
 import urllib
-from json import loads
+import json
+
+# Configs
+LIBREY_INSTANCE = '' # URL of LibreY instance
+LIBREY_INSTANCE_API = urllib.parse.urljoin(LIBREX_INSTANCE, 'api.php?q=%')
+# Locales
+LOADING_TEXT = "<b>🔎 Поиск...</b>"
+ERROR_TEXT = "<b>⛔️ Не удалось выполнить поисковой запрос.</b>\n"
+ERROR_URLError_TEXT = f"Невозможно подключиться к <a href='{LIBREY_INSTANCE}'>серверу.</a>"
+ERROR_JSONDecodeError_TEXT = "Ошибка при обработке JSON-ответа от API."
+UNKNOWN_ERROR_REASON = "Неизвестная ошибка. Код ошибки: "
 
 @dp.message_handler(commands='s')
 async def search(message: types.Message):
-    results_json = loads(urllib.request.urlopen(f'{librex_instance}{urllib.parse.quote(message.text[3:])}').read())
-    result_text = ''
-    for result in results_json:
-        if 'special_response' not in result:
-            result_text += f'\n\n<b><a href="{result["url"]}">{result["title"]}</a></b>\n{result["description"]}'
-        else:
-            result_text += f'\n\n<i><a href="{result["special_response"]["source"]}">{result["special_response"]["response"]}</a></i>'
-    await message.reply(result_text, parse_mode='HTML')
+    msg = await message.reply(LOADING_TEXT, parse_mode="HTML")
+    try:
+        results_json = json.loads(urllib.request.urlopen(f'{LIBREY_INSTANCE_API}{urllib.parse.quote(message.text[3:])}').read())
+        result_text = ''
+        for result in results_json:
+            if 'special_response' not in result:
+                result_text += f'\n\n<b><a href="{result["url"]}">{result["title"]}</a></b>\n{result["description"]}'
+            else:
+                result_text += f'\n\n<i><a href="{result["special_response"]["source"]}">{result["special_response"]["response"]}</a></i>'
+        await msg.edit_text(result_text, parse_mode='HTML')
+    except urllib.error.URLError:
+        await msg.edit_text(f"{ERROR_TEXT}{ERROR_URLError_TEXT}", parse_mode="HTML")
+    except json.JSONDecodeError:
+        await msg.edit_text(f"{ERROR_TEXT}{ERROR_JSONDecodeError_TEXT}", parse_mode="HTML")
+    except Exception as error:
+        error_name = error.__class__.__name__
+        error_context = error.args[0]
+        await msg.edit_text(f"{ERROR_TEXT}{UNKNOWN_ERROR_REASON}{error_name}: {error_context}", parse_mode="HTML")
